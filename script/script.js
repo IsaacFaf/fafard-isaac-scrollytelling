435aaf16641192ebb8996fcd51c809d1d3165532
IsaacFaf <89608287+IsaacFaf@users.noreply.github.com>
2023-04-23 21:16:56 -0400

    CodeImplemant

---
+++ script/script.js
@@ -0,0 +1,17 @@
+const scrollDown = document.querySelector('.scroll-down');
+const height = window.innerHeight;
+let lastScrollPos = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScrollPos > lastScrollPos && currentScrollPos > height) {
+    scrollDown.classList.add('hidden');
+  } else {
+    scrollDown.classList.remove('hidden');
+  }
+  lastScrollPos = currentScrollPos;
+});
+
+window.addEventListener('load', () => {
+  scrollDown.style.opacity = '1';
+});
