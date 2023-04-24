0754fde9ece3a920cf6f616e1f69395df6c60402
IsaacFaf <89608287+IsaacFaf@users.noreply.github.com>
2023-04-24 01:09:12 -0400

    2.2a Fini
    Je peux maintenant commencer la partie B

--- script/script.js
+++ script/script.js
@@ -19,3 +19,53 @@ window.addEventListener('load', () => {
 
 
 //SPRITESHEEET
+
+let isScrolling; //Déclaration pour le setTimeout
+window.addEventListener('scroll', function() {
+  let sprite = document.querySelector('.image');
+  
+  let spritePos = sprite.getBoundingClientRect().top;
+  let scrollPos = window.innerHeight;
+  
+// au Scroll ajout de class
+  if (spritePos <= scrollPos){
+    
+    sprite.style.animationPlayState = 'running';
+    body.classList.add('is-scrolling');
+    console.log("Je scroll")
+  } else {
+    
+    sprite.style.animationPlayState = 'paused';
+    body.classList.remove('is-scrolling')
+    console.log("Code éxécuté")
+  }
+  //Variable de temps
+  this.window.clearTimeout(isScrolling); //Annuler le minuteur
+  isScrolling = setTimeout(function(){
+    document.body.classList.remove('is-scrolling');
+    minuteur = "Valeur de ma variable";
+    console.log(minuteur);
+  }, 100);
+}, false);
+
+
+/*const img = document.querySelector('.image');
+let timeL = gsap.timeline({
+  scrollTrigger:{
+    trigger:Image,
+    start:'top center+=200',
+    end:'bottom center-=200',
+    onEnter:() =>{
+      document.body.classList.add('is-scrolling');
+      timeL.play();
+    },
+    onLeaveBack: () => {
+      document.body.classList.remove('is-scrolling');
+      timeL.pause();
+    }
+  }
+});
+timeL.to('.image', {backgroundPositionX:'-4096', duration:0.2, repeat:-1})
+
+*/
+
