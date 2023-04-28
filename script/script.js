1b1f275ca7049f4bf76231455cea82ff108d3fe4
IsaacFaf <89608287+IsaacFaf@users.noreply.github.com>
2023-04-27 20:02:46 -0400

    gsap 2 timeline et 1 animation Pour total 5 anim

--- script/script.js
+++ script/script.js
@@ -110,3 +110,37 @@ timeL.to('.image', {backgroundPositionX:'-4096', duration:0.2, repeat:-1})
 
 */
 
+//TimeLine
+
+
+
+
+
+//INSTRUCTION 4 - Animation de base de quelques images
+
+let swirdArr = document.querySelectorAll("#chapitre-2 img");
+
+//Chap2
+gsap.fromTo(swirdArr, {scaleY: "100%", scaleX: "100%", opacity:"100%" }, { 
+  scaleY: "1%", scaleX: "1%", duration:2.2, repeat:-1,yoyo:true, opacity:"36%"
+});
+//Chap5
+gsap.timeline().fromTo('#chapitre-5 img.a',{x:'0%', y:"0%"}, 
+{x: '35vw', y:"20vh",rotate:"6deg", duration: 2})
+.to('#chapitre-5 img.a', { x: '-35vw', y:"20vh", rotate:"30deg", duration: 2})
+.to('#chapitre-5 img.a', { x: '35vw', y:"-20vh", rotate:"-375deg", duration: 2})
+.to('#chapitre-5 img.a', { x: '-35vw', y:"-20vh", rotate:"5deg", duration: 2})
+.to('#chapitre-5 img.a', { x: '0vw', y:"-25vh", rotate:"25deg",duration:2})
+.to('#chapitre-5 img.a', { x: '0vw', y:"0vw", rotate:"0deg", duration: 2})
+.to('#chapitre-5 img.a', { scaleX:'150%', scaleY:'150%',rotate:"360deg", duration: 2, repeat:-1})
+
+
+//Chap6
+gsap.timeline().fromTo('#chapitre-6 div.a', {x:'-15vw', y:"-120px",opacity:"0%",ease:'none', duration:0.8},
+{x: '-9vw', y:"-58px",rotate:"6deg",opacity:"100%", duration: 1.1, ease:'none'})
+.to('#chapitre-6 div.a', { x: '-2vw', y:"-10px", rotate:"30deg", duration: 0.95,ease:'none'})
+.to('#chapitre-6 div.a', { x: '5.5vw', y:"55px", rotate:"45deg", duration: 0.88,ease:'none'})
+.to('#chapitre-6 div.a', { x: '10.8vw', y:"108px", rotate:"52deg", duration: 0.86,ease:'none'})
+.to('#chapitre-6 div.a', { x: '15.8vw', y:"188px", rotate:"58deg", duration: 0.82,ease:'none',})
+.to('#chapitre-6 div.a', { x: '17.42vw', y:"213px", rotate:"60deg", duration: 0.8,ease:'none'})
+.to('#chapitre-6 div.a', { x: '17.42vw', y:"163px", rotate:"60deg", duration: 1, opacity:"1%"})
